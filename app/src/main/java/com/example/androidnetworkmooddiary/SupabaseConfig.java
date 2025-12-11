a1e28ba4febbc4559ece7b1a705379a335cf6a87
gress <gress_apple@mail.ru>
2025-12-11 15:04:39 +0600

    Добавлен класс SupabaseConfig

---
+++ app/src/main/java/com/example/androidnetworkmooddiary/SupabaseConfig.java
@@ -0,0 +1,13 @@
+package com.example.androidnetworkmooddiary;
+
+public class SupabaseConfig {
+    public static final String SUPABASE_URL = "https://sjjisresdqkcmjlztxbh.supabase.co";
+    public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqamlzcmVzZHFrY21qbHp0eGJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MDM2MDYsImV4cCI6MjA4MDk3OTYwNn0.dq1PZDCMl9TMWTJFZVkke-ksH4At3anOI6GrqbLxQ9E";
+    public static final String AUTH_SIGNUP_URL =
+            SUPABASE_URL + "/auth/v1/signup";
+    public static final String AUTH_SIGNIN_URL =
+            SUPABASE_URL + "/auth/v1/token?grant_type=password";
+    public static final String TABLE_URL =
+            SUPABASE_URL + "/rest/v1/MoodDiary";
+
+}
